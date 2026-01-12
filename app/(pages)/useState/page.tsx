ac6501a9a728cec462468b596a034e649596bcab
Javier Pernía <javier.perniac@gmail.com>
2026-01-12 15:42:28 -0400

    primer commit te practicas de react

---
+++ app/(pages)/useState/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useState } from 'react';
+
+interface User {
+    name: string;
+    age: number;
+    email: string;
+}
+
+export default function UseStatePage() {
+    const [count, setCount] = useState<number>(0);
+    const [name, setName] = useState<string>('');
+    const [users, setUsers] = useState<string[]>([]);
+    const [usersVals, setUsersVals] = useState<string>('');
+    const [user, setUser] = useState<User>({
+        name: '',
+        age: 0,
+        email: '',
+    });
+
+
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>Add</button><br />
+            <button onClick={() => count > 0 && setCount(count - 1)}>Sub</button>
+
+            <p>Nombre: {name}</p>
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+
+            <p>Users: {users.join(', ')}</p>
+            <input type="text" value={usersVals} id="users" onChange={(e) => setUsersVals(e.target.value)} />
+            <button onClick={() => setUsers([...users, usersVals])}>Add user</button>
+            <button onClick={() => setUsers([])}>Clear</button>
+
+            <p>User: {user.name}</p>
+            <input type="text" value={user.name} id="username" onChange={(e) => setUser({ ...user, name: e.target.value })} />
+            <button onClick={() => setUser({ name: '', age: 0, email: '' })}>Clear</button>
+        </div>
+    );
+}
