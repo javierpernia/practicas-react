cc4946e4f320e7aff06f27c873dadfc96e651003
Javier Pernía <javier.perniac@gmail.com>
2026-01-16 11:35:16 -0400

    se acomodó el código de useReducer para ser mas lejible y poderlo analizar mas fácil

--- app/(pages)/useReducer/page.tsx
+++ app/(pages)/useReducer/page.tsx
@@ -16,13 +16,20 @@ type Action =
 function todoReducer(state: Todo[], action: Action): Todo[] {
   switch (action.type) {
     case 'ADD':
-      return [...state, { id: Date.now(), text: action.payload, completed: false }];
+      return [
+        ...state,
+        { id: Date.now(), text: action.payload, completed: false }];
+
     case 'TOGGLE':
       return state.map(todo =>
-        todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo
+        todo.id === action.payload
+          ? { ...todo, completed: !todo.completed }
+          : todo
       );
+
     case 'DELETE':
       return state.filter(todo => todo.id !== action.payload);
+
     default:
       return state;
   }
@@ -38,6 +45,7 @@ export default function TodoApp() {
       dispatch({ type: 'ADD', payload: input.trim() });
       setInput('');
     }
+    console.log(todos);
   };
 
   return (
